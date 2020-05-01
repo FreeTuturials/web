0178dba78198a2656598c471d41e0f2bf46ddb72
Arrow6 <48833849+Arrow6@users.noreply.github.com>
2020-05-02 00:04:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function veiw() {
+    console.log("veiw");
+    link = document.createElement('a');
+    link.href = "veiw.html"
+    link.click()
+}
+
+function support() {
+    console.log("support");
+    link = document.createElement('a');
+    link.href = "support.html"
+    link.click()
+}
